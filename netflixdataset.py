9c5e00ae0e36310bcaaee9c249895c539b91e9b9
Eloisa Espano <125548778+eeloisaje@users.noreply.github.com>
2024-10-09 21:45:48 +0800

    Dashboard Updated - goods na  *~*

--- netflixdataset.py
+++ netflixdataset.py
@@ -175,9 +175,9 @@ with OverviewTab:
 
     st.write("### First 5 Rows of the Dataset")
     st.write(df_cleaned.head())
-
-    with DashboadTab:
-        st.markdown("<h3><span style='color:#E50914'>Netflix</span> Data Visualization of 2015 - 2021</h3>", unsafe_allow_html=True)
+    
+   with DashboadTab:
+        st.markdown("<h3><span style='color:red'>Netflix</span> Data Visualization of 2005 - 2021</h3>", unsafe_allow_html=True)
         type_count = df.groupby(['release_year', 'type']).size().reset_index(name='count')
 
         # Separate the data for Movies and TV Shows
@@ -209,7 +209,7 @@ with OverviewTab:
 
         # Update layout for the chart
         fig.update_layout(
-            title="Number of Movies and TV Shows by Release Year (2015 and 2021)",
+            title="Number of Movies and TV Shows by Release Year (2005 and 2021)",
             xaxis_title='Release Year',
             yaxis_title='Number of Titles',
             template='plotly',
@@ -219,10 +219,10 @@ with OverviewTab:
         # Display the plot in Streamlit
         st.plotly_chart(fig)
 
-        st.markdown("<i><small>📈 Figure 1: This chart illustrates the trends in Netflix releases for Movies and TV Shows from 2015 to 2021.</small></i>", unsafe_allow_html=True)
+        st.markdown("<i><small>📈 Figure 1: This chart illustrates the trends in Netflix releases for Movies and TV Shows from 2005 to 2021.</small></i>", unsafe_allow_html=True)
         st.markdown("<small><b>Movies:</b> The number of movie releases surged significantly between 2016 and 2017, with the highest count in 2017 at 729 titles. After 2017, the number of movie releases decreased steadily, with a sharp drop after 2019.</small>", unsafe_allow_html=True)
         st.markdown("<small><b>TV Shows</b>: While starting with lower numbers, TV show releases gradually increased, peaking in 2020 with 391 titles. The rise was more consistent compared to movies, but there was a sharp decline post-2020.</small>", unsafe_allow_html=True)
-        st.markdown("<small><b>General Trend:</b> Movies dominated in volume throughout the period, but TV Shows saw steady growth, particularly from 2015 onward. The decline in both categories after 2020 may reflect broader industry shifts or impacts, potentially due to the global pandemic.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>General Trend:</b> Movies dominated in volume throughout the period, but TV Shows saw steady growth, particularly from 2005 onward. The decline in both categories after 2020 may reflect broader industry shifts or impacts, potentially due to the global pandemic.</small>", unsafe_allow_html=True)
         
          # Plot : Movies released by year
         movies = df[df['type'] == 'Movie']
@@ -239,21 +239,33 @@ with OverviewTab:
         top_10_countries = movies['country'].value_counts().nlargest(10).reset_index()
         top_10_countries.columns = ['country', 'count'] 
         fig_treemap = px.treemap(top_10_countries, path=['country'], values='count',
-                                title='Top 10 Countries by Number of Movies Produced (2015-2021)',
+                                title='Top 10 Countries by Number of Movies Produced (2005-2021)',
                                 color='count',
                                 color_continuous_scale=color_scale)
 
         st.plotly_chart(fig_treemap)
 
+        st.markdown("<i><small>📈 Figure 2: This treemap visualization highlights the Top 10 Countries by Number of Movies Produced from 2005 to 2021 on Netflix. </small></i>", unsafe_allow_html=True)
+        st.markdown("<small><b>United States:</b> Leading the global movie production with 2001 titles, the U.S. dominates the chart, significantly outpacing other countries.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>United Kingdom and Canada:</b> These countries follow, but with much smaller contributions compared to the U.S., showing moderate movie production in the period.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>France, Nigeria, and Egypt:</b> These countries also make notable contributions, with a sizable share of Netflix movie releases.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>Spain, Mexico, and Indonesia:</b> These countries contribute fewer movies, rounding out the list of top 10 movie-producing countries.</small>", unsafe_allow_html=True)
+
          # Treemap for TV Shows 
         top_10_countries = tv_shows['country'].value_counts().nlargest(10).reset_index()
         top_10_countries.columns = ['country', 'count'] 
         fig_treemap = px.treemap(top_10_countries, path=['country'], values='count',
-                                title='Top 10 Countries by Number of TV Shows Produced (2015-2021)',
+                                title='Top 10 Countries by Number of TV Shows Produced (2005-2021)',
                                 color='count',
                                 color_continuous_scale=px.colors.sequential.Plasma[::-1])
 
         st.plotly_chart(fig_treemap)
+
+        st.markdown("<i><small>📈 Figure 3: This treemap visualization highlights the Top 10 Countries by Number of TV Shows Produced from 2005 to 2021 on Netflix. Key insights include: </small></i>", unsafe_allow_html=True)
+        st.markdown("<small><b>United States:</b> The U.S. leads significantly with 806 TV shows, making it the dominant country in Netflix's TV show production.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>United Kingdom:</b> These countries follow, with 238 TV shows, making it second on the list.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>Japan and South Korea:</b> Following the U.K., Japan produced 163 TV shows, while South Korea contributed 164 TV shows, showcasing a strong presence in the Asian entertainment industry.</small>", unsafe_allow_html=True)
+        st.markdown("<small><b>Canada, India, France, Australia, Taiwan, and Spain:</b> These countries also made notable contributions, with varying levels of production but still placing within the top 10.</small>", unsafe_allow_html=True)
 
         # Plot : Distribution of ratings
         rating_count = df['rating'].value_counts().reset_index()
@@ -261,6 +273,10 @@ with OverviewTab:
         fig2 = px.pie(rating_count, values='count', names='rating', title="Distribution of Netflix Ratings")
         st.plotly_chart(fig2)
 
+        st.markdown("<i><small>📊 **Figure 3**: This treemap provides an overview of the **Top 10 Countries** by the number of **TV Shows Produced on Netflix** between **2005 and 2021**. The United States leads significantly, followed by Japan and South Korea, underscoring the dominance of North America and Asia in Netflix's TV show production.</small></i>", unsafe_allow_html=True)
+        st.markdown("<small>The pie chart on Netflix ratings distribution reveals that TV-MA (Mature Audiences) dominates with 39.4% of all rated content, indicating that a significant portion of Netflix's content is tailored to adult viewers. TV-14, targeting teenagers, follows with 24.3%, reflecting a large amount of content for younger audiences as well. TV-PG, at 9.7%, and R-rated content, at 8.62%, further contribute to the platform's adult-focused offerings. The presence of TV-Y (3.09%) and TV-Y7 (3.06%) shows Netflix's smaller but notable commitment to children’s programming. The lower percentages for PG (2.9%) and TV-G (2.48%) suggest that family-oriented content forms a smaller portion of the overall library. This data highlights Netflix’s emphasis on mature and teen audiences while maintaining a diverse range of content for different age groups.</small>", unsafe_allow_html=True)
+
+     
         # Plot: Distribution of Top Genres : Movies
         country_genre_counts = movies.groupby(['country', 'listed_in']).size().unstack(fill_value=0)
         top_countries = country_genre_counts.sum(axis=1).sort_values(ascending=False).head(10).index
@@ -281,7 +297,7 @@ with OverviewTab:
 
         # Update layout for the stacked bar chart
         fig6.update_layout(
-            title='Distribution of Top Genres on Movies in Top 10 Countries',
+            title='Distribution of Top Genres on Movies in Top 10 Countries (2005-2021)',
             xaxis_title='Country',
             yaxis_title='Number of Movies',
             barmode='stack',
@@ -291,6 +307,8 @@ with OverviewTab:
 
         # Display the plot in Streamlit
         st.plotly_chart(fig6)
+        st.markdown("<i><small>📊 **Figure**: This stacked bar chart highlights the **Top Genres** in the **Top 10 Countries** for Netflix movies. </small></i>", unsafe_allow_html=True)
+        st.markdown("<small>The US dominates with **Documentaries** (265) and **Stand-Up Comedy** (196), showcasing a strong focus on factual and comedic content. **India** leans toward a mix of **Comedies, Dramas, and International Movies**, reflecting its diverse storytelling preferences. The **UK** favors **Documentaries**, while **Canada** balances **family-friendly comedies** and **Documentaries**. **France** highlights a preference for **Dramas** and **International Movies**, indicating a taste for emotionally rich and globally diverse content. Each country's top genres reflect its unique cultural and entertainment priorities.</small>", unsafe_allow_html=True)
 
         # Plot: Distribution of Top Genres : TV Shows 
         country_genre_counts = tv_shows.groupby(['country', 'listed_in']).size().unstack(fill_value=0)
@@ -299,11 +317,11 @@ with OverviewTab:
         top_genres = top_country_genre.sum().sort_values(ascending=False).head(10).index
 
         top_country_genre = top_country_genre[top_genres]
-        fig6 = go.Figure()
+        fig7 = go.Figure()
 
         # Loop through each genre to add a trace to the figure
         for genre in top_country_genre.columns:
-            fig6.add_trace(go.Bar(
+            fig7.add_trace(go.Bar(
                 x=top_country_genre.index,
                 y=top_country_genre[genre],
                 name=genre,
@@ -311,24 +329,20 @@ with OverviewTab:
             ))
 
         # Update layout for the stacked bar chart
-        fig6.update_layout(
-            title='Distribution of Top Genres on TV Shows in Top 10 Countries',
+        fig7.update_layout(
+            title='Distribution of Top Genres on TV Shows in Top 10 Countries (2005-2021)',
             xaxis_title='Country',
-            yaxis_title='Number of Movies',
+            yaxis_title='Number of TV Shows',
             barmode='stack',  # Set barmode to stack
             legend_title='Genre',
             template='plotly'
         )
 
-#         # Display the plot in Streamlit
-        st.plotly_chart(fig6)
+            # Display the plot in Streamlit
+        st.plotly_chart(fig7)
+        st.markdown("<i><small>📊 **Figure**: This bar chart presents the **Top Genres** in selected countries for Netflix shows.</small></i>", unsafe_allow_html=True)
+        st.markdown("<small>The **US** leads with a significant preference for **Kid's TV** (93), showing a strong demand for child-friendly content, followed by **Reality TV** (72), and both **Docuseries** and **TV Comedies** (60 each). **South Korea** emphasizes a mix of **International, Korean, and Romantic TV Shows** (63), reflecting its global influence and local storytelling. **Japan** stands out with a strong affinity for **Anime** and **International TV Shows** (71), indicating the popularity of animated content and cross-border entertainment.</small>", unsafe_allow_html=True)
 
-#     with col2:
-#         # Descriptions and Data Analysis
-#         st.write("""
-#         """)
-
-# Analysis Tab
     with AnalysisTab:
         st.write("### Analysis")
         st.markdown(
